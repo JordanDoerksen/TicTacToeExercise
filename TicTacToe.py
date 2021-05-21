6ed8641178b317b490d1fa57968b941597e7f9f3
Jordan Doerksen <jdoerksen@propaksystems.com>
2021-05-21 12:30:08 -0600

    Players turn and print board is now working

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,14 +1,25 @@
 import copy
+import os
+
 ##TESTED AND WORKING
+
+#Clear the console
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
 rows = 3  # int(input("How many rows would you like? "))
 cols = 3  # int(input("How many cols would you like? "))
 board = [[0 for i in range(cols)] for j in range(rows)]
+# board[row,col]
 
 x = 1
 o = 2
+gamePiece = {1: "X", 2: "O"}
 
 
-# board[row,col]
+
 
 
 # returns 0 if no winner
@@ -52,6 +63,7 @@ def getWinner(board):
             return tempDiag[0]
     return 0
 
+
 def playAllGames(currentBoard, player, tie=0, xWins=0, oWins=0):
     tempWinner = getWinner(currentBoard)
     otherPlayer = x if player == o else o
@@ -61,23 +73,82 @@ def playAllGames(currentBoard, player, tie=0, xWins=0, oWins=0):
                 if currentBoard[row][col] == 0:
                     tempBoard = copy.deepcopy(currentBoard)
                     tempBoard[row][col] = player
-                    tie, xWins, oWins = playAllGames(tempBoard,otherPlayer,tie,xWins,oWins)
+                    tie, xWins, oWins = playAllGames(tempBoard, otherPlayer, tie, xWins, oWins)
     elif tempWinner == 1:
         return tie, xWins + 1, oWins
     elif tempWinner == 2:
         return tie, xWins, oWins + 1
     elif tempWinner == 4:
         return tie + 1, xWins, oWins
-    return tie,xWins,oWins
+    return tie, xWins, oWins
 
+def print_board(board):
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            print(value if value != 0 else ((i*3) + (j+1)), end='')
+            print("|" if j < cols - 1 else "\n", end='')
+        print('—'*(cols*2-1))
 
-# print board
-
+def player_goes(board, player):
+    print_board(board)
+    print(f"Your turn place your {gamePiece[player]}")
+    validMove = False
+    move = None
+    moveRow = None
+    moveCol = None
+    while not validMove:
+        move = int(input("Where would you like to play? "))
+        moveRow = (move - 1) // 3
+        moveCol = (move - 1) % 3
+        if(board[moveRow][moveCol] == 0):
+            validMove = True
+        else:
+            cls()
+            print_board(board)
+            print("You cannot play there?")
+    board[moveRow][moveCol] = gamePiece[player]
+    return board
 
 
 #########WORKING ON
 
 
 
+def computer_goes(board, player):
+    print(f"Computer is playing {gamePiece[player]}")
+    return board
+
+def play_pvp_game(board):
+    cls()
+    print("You will now play a game against another person")
 
 
+def play_pvc_game(board):
+    cls()
+    print("You will now play against a computer. Good Luck...")
+    print("Would you like to play as x or o? x goes first\nx : 1\no : 2")
+    player = int(input("Selection: "))
+    computer = x if player == 2 else o
+    cls()
+    print(f"You have chosen to play as {gamePiece[player]} you go {'first' if player == 1 else 'last'}")
+    turn = 0
+    while turn < 9: #getWinner(board) == 0:
+        if (turn % 2 == 0 and player == 1) or (turn % 2 == 1 and player == 2):
+            board = player_goes(board, player)
+            turn += 1
+        else:
+            board = computer_goes(board, computer)
+            turn += 1
+
+
+
+cls()
+# 0 = PvC
+# 1 = PvP
+gameMode = 0  # int(input("Would you like to play against a computer or another human?\n0 : vs. Computer\n1 : vs.
+# User\nSelection: "))
+
+if gameMode:
+    play_pvp_game(board)
+else:
+    play_pvc_game(board)
